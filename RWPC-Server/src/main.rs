dc1e868f08cbc4e1842ac536bb2eab0cb6966693
BlackdBlook <zhangshihao2456@yahoo.com>
2024-05-11 10:13:59 +0800

    server

--- RWPC-Server/src/main.rs
+++ RWPC-Server/src/main.rs
@@ -1,5 +1,10 @@
 use std::net::UdpSocket;
 use std::str;
+use std::process::Command;
+fn is_awake_str(str : &str) -> bool{
+    return str == "123";
+}
+
 
 fn main() -> std::io::Result<()> {
     // 创建一个UDP Socket并绑定到本地端口51360
@@ -14,5 +19,21 @@ fn main() -> std::io::Result<()> {
 
         // 打印消息和发送者的地址
         println!("Received from {}: {}", src, text);
+
+        if is_awake_str(&text) {
+            // 如果是，执行控制台指令echo 123
+            Command::new("cmd")
+            .arg("/C")
+            .arg("/usr/bin/etherwake -D -i \"br-lan\" \"58:11:22:A2:54:23\"")
+            .output()
+            .expect("Failed to execute command");
+            let callback_str = String::from("ServerCallback Successful");
+            
+            match socket.send_to(callback_str.as_bytes(), src){
+                Ok(size)=> println!("Send Callback {}", size),
+                Err(e) => println!("Send Callback Failed {}", e)
+            }
+            
+        }
     }
 }
